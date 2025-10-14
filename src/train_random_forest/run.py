d04686c18b7bc6090f4e6ff95326c643f5313bc0
Jasmine Arellano <jasmineguerra178@gmail.com>
2025-10-14 13:51:29 -0500

    Fixing syntax error

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -173,7 +173,7 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # 2 - A OneHotEncoder() step to encode the variable
     non_ordinal_categorical_preproc = make_pipeline(
         # YOUR CODE HERE
-        SimpleImputer(strategy="most_frequent")
+        SimpleImputer(strategy="most_frequent"),
         OneHotEncoder()
     )
     ######################################
